285be1565552b087d9f3e5b52dd963369a7b3ff7
JPrietoDLC <JPrietoC@uxcale.com>
2024-09-16 21:29:36 +0200

    Bug Fix on Experience

--- src/modules/candidates/widgets/add-experience/add-experience.widget.tsx
+++ src/modules/candidates/widgets/add-experience/add-experience.widget.tsx
@@ -133,7 +133,9 @@ export function AddExperienceWidget(props: AddExperienceWidgetProps) {
                 position: position,
                 company: company,
                 startedDate: startedDate + dateTimeSuffix,
-                finishedDate: finishedDate + dateTimeSuffix,
+                finishedDate: finishedDate
+                    ? finishedDate + dateTimeSuffix
+                    : null,
             },
             resourceId: experienceId,
             candidateId: props.candidateId,
@@ -296,7 +298,7 @@ export function AddExperienceWidget(props: AddExperienceWidgetProps) {
                                             viewBox="0 0 24 24"
                                             stroke-width="1.5"
                                             stroke="currentColor"
-                                            className="size-4 mr-2"
+                                            className="size-4"
                                         >
                                             <path
                                                 stroke-linecap="round"
@@ -304,7 +306,6 @@ export function AddExperienceWidget(props: AddExperienceWidgetProps) {
                                                 d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
                                             />
                                         </svg>
-                                        Update
                                     </button>
                                 )}
                             </div>
